25434a201c88d48591cdd7161627a60d1cbd164f
dextraise <gilles@teerex.ovh>
2022-04-10 22:11:46 +0200

    trying to get it to run

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/plan-broderie'));
+app.use(express.static(__dirname + '/dist/planbroderie'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/plan-broderie/index.html'));});
+    '/dist/planbroderie/index.html'));});
 app.listen(process.env.PORT || 4200);
